903350708db43cc914521f952a097ef011d1fe4d
Code Mountains <thecodemountains@gmail.com>
2023-11-02 23:49:18 -0500

    terraform cloud connection

--- main.tf
+++ main.tf
@@ -68,6 +68,15 @@ resource "aws_security_group" "web-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  // Additional rule for SSH traffic on port 22
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
     from_port   = 0
